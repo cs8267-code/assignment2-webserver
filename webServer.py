f4f66b16a760bd2f3369f9939f38c0ae2e330a33
somerscyn <csomers@loyola.edu>
2026-02-10 21:37:58 -0500

    tweak

--- webServer.py
+++ webServer.py
@@ -32,8 +32,8 @@ def webServer(port=13331):
       #Fill in start 
               
       #Content-Type is an example on how to send a header as bytes. There are more!
-      outputdata += b"HTTP/1.1 200 OK\r\n" #header for a response that is okay
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata = b"HTTP/1.1 200 OK\r\n" #header for a response that is okay
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
       outputdata += b"\r\n"
 
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
@@ -58,8 +58,8 @@ def webServer(port=13331):
       # Send response message for invalid request due to the file not being found (404)
       # Remember the format you used in the try: block!
       #Fill in start
-      outputdata += b"HTTP/1.1 404 Not Found\r\n" #header for a response that file is not found
-      outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+      outputdata = b"HTTP/1.1 404 Not Found\r\n" #header for a response that file is not found
+      outputdata += b"Content-Type: text/html; charset=UTF-8\r\n"
       outputdata += b"\r\n"
       #Fill in end
       connectionSocket.send(outputdata)
